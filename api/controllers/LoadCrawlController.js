dab9c3e37fa917982760dd0c4e7cf9972e272867
martinhecher <martin.hecher@fraunhofer.at>
2015-11-09 16:13:24 +0100

    return sorted and sliced candidates array

--- api/controllers/LoadCrawlController.js
+++ api/controllers/LoadCrawlController.js
@@ -9,15 +9,29 @@ var FocusedCrawler = require('../../bindings/FocusedCrawler/app');
 
 module.exports = {
   create: function(req, res, next) {
-    var crawlId = req.param('crawl_id');
+    var crawlId = req.param('crawl_id'),
+      count = req.param('count') || 50;
 
     console.log('Load crawl: ' + crawlId);
+    console.log('count: ' + count);
 
     var focusedCrawler = new FocusedCrawler({
       baseURL: 'http://asev.l3s.uni-hannover.de:9986/api/CrawlAPI/'
     });
     focusedCrawler.loadCrawl(crawlId).then(function(candidates) {
-      res.send(candidates);
+      candidates = JSON.parse(candidates);
+      candidates.sort(function(a, b) {
+        if (a.count > b.count) {
+          return 1;
+        };
+        if (a.count < b.count) {
+          return -1;
+        };
+
+        return 0;
+      });
+      var result = candidates.slice(0, count);
+      res.send(result);
     });
   }
 };
